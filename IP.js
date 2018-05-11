dab0d57245bc65a39c0eb70a13bc14368b0ba2dc
joeydeeds <joey.deeds@gmail.com>
2018-05-11 12:34:01 -0500

    error handling for manual

--- IP.js
+++ IP.js
@@ -1,3 +1,3 @@
-const IP = `http://167c8c69.ngrok.io/`;
+const IP = `http://036aaf82.ngrok.io/`;
 
 export default IP;
--- components/screens/ManualEntry.js
+++ components/screens/ManualEntry.js
@@ -3,74 +3,92 @@ import { TextInput } from 'react-native'
 import axios from 'axios'
 import { Text, Container, Picker, List, ListItem, Content } from 'native-base'
 
-
 export default class ManualEntry extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            text: '',
-            nutrition: [],
-            foodType: 'Common Foods'
-        }
-        this.onSubmitFood = this.onSubmitFood.bind(this)
-      }
-
-      onSubmitFood() {
-                let result = axios.get(
-                    `http://167c8c69.ngrok.io/api/nutri/search/${
-                        encodeURI(this.state.text)
-                    }`
-                )
-                .then((result) => {
-                    console.log("on submit food!", result.data)
-                    this.setState({
-                        nutrition:result.data
-                    })
-                })
-                .catch((error) => {
-                    console.error(error)
-                    return <Text>{error.message}</Text>
-                })
-
-        this.setState({ text: '' })
-      }
-
-    render () {
-        return (
-            <Container style={styles.container}>
-                <Text>What did you put in your face hole?</Text>
-                <TextInput style={{height: 40, borderColor: 'gray', borderWidth: 1}}
-                    onChangeText={(text) => this.setState({text})}
-                    value={this.state.text}
-                    placeholder={'ENTER FOOD HERE'}
-                    onSubmitEditing={this.onSubmitFood}
-                    name='food'
-                    />
-                    <Container>
-                    <Content>
-                    {
-                        this.state.nutrition.map((food, index) => {
-                            return (
-                            <List key={index}>
-                              <ListItem itemDivider><Text>name:  {food.name}</Text></ListItem>
-                              <ListItem><Text>sugar:  {food.sugar}</Text></ListItem>
-                              <ListItem><Text>calories:  {food.calories}</Text></ListItem>
-                              <ListItem><Text>total fat:  {food.totalFat}</Text></ListItem>
-                              <ListItem><Text>carbs:  {food.carbs}</Text></ListItem>
-                           </List>
-                            )
-                        })
-                    }
-                </Content>
-                </Container>
-            </Container>
-        )
+  constructor(props) {
+    super(props)
+    this.state = {
+      text: '',
+      nutrition: [],
+      foodType: 'Common Foods',
+      error: ''
     }
+    this.onSubmitFood = this.onSubmitFood.bind(this)
+  }
+
+  onSubmitFood() {
+    this.setState({ error: '' })
+    let result = axios
+      .get(
+        `http://036aaf82.ngrok.io/api/nutri/search/${encodeURI(
+          this.state.text
+        )}`
+      )
+      .then(result => {
+        if (result.data === 'Item was not found! Try again') {
+          this.setState({ error: result.data })
+        } else {
+          this.setState({
+            nutrition: result.data
+          })
+        }
+      })
+      .catch(error => {
+        console.error(error)
+        return <Text>{error.message}</Text>
+      })
+
+    this.setState({ text: '' })
+  }
+
+  render() {
+    return (
+      <Container style={styles.container}>
+        <Text>What did you put in your face hole?</Text>
+        <TextInput
+          style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+          onChangeText={text => this.setState({ text })}
+          value={this.state.text}
+          placeholder={'ENTER FOOD HERE'}
+          onSubmitEditing={this.onSubmitFood}
+          name="food"
+        />
+        <Container>
+          <Content>
+            {this.state.error ? (
+              <Text>{this.state.error}</Text>
+            ) : (
+              this.state.nutrition.map((food, index) => {
+                return (
+                  <List key={index}>
+                    <ListItem itemDivider>
+                      <Text>name: {food.name}</Text>
+                    </ListItem>
+                    <ListItem>
+                      <Text>sugar: {food.sugar}</Text>
+                    </ListItem>
+                    <ListItem>
+                      <Text>calories: {food.calories}</Text>
+                    </ListItem>
+                    <ListItem>
+                      <Text>total fat: {food.totalFat}</Text>
+                    </ListItem>
+                    <ListItem>
+                      <Text>carbs: {food.carbs}</Text>
+                    </ListItem>
+                  </List>
+                )
+              })
+            )}
+          </Content>
+        </Container>
+      </Container>
+    )
+  }
 }
 
 const styles = {
-    container: {
-        margin: 20,
-        marginTop: 50
-    }
+  container: {
+    margin: 20,
+    marginTop: 50
+  }
 }
